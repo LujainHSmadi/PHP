753f5e2bef97bc7e77ab24be64cb8bc10455c381
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-04-25 20:56:44 +0300

    main

--- PHPTask_24-April-2022/Task1.php
+++ PHPTask_24-April-2022/Task1.php
@@ -37,6 +37,7 @@ function seasone($temp){
 }
 
 seasone(27);
+// **********************************
 
 function sum($firstInteger  , $secondInteger ){
 
@@ -46,24 +47,29 @@ if($firstInteger == $secondInteger )
 }
 
 echo(sum(2,2));
+// **********************************
 
 function sum2($firstInteger,$secondInteger){
     if(($firstInteger + $secondInteger)==30){
-        echo "false <br>";
+        echo " <br>false <br>";
     }
+    else
+    echo "<br> true <br>";
 }
 sum2(10,10);
+// **********************************
 
 function positive($number){
     if($number%3 > 0){
-        return true;
+        return "true";
     }
     else
-    return flase;
+    return "flase";
 
 }
 
-echo "<br>".positive(20);
+echo "<br>".positive(20)."<br>";
+// **********************************
 
 function num($num){
     if($num >20 && $num<50){
@@ -71,7 +77,118 @@ function num($num){
     }
     else return flase;
 }
+// **********************************
 
-function largest_number(){
+function largest_number($arr){
+    $max = $arr[0];
+    for($i = 1; $i<3;$i++){
+        if($max<$arr[$i])
+        $max = $arr[$i];
+        
+
+    }
+    return $max;
+
+}
+$arr1 = array(1,5,9);
+echo largest_number($arr1)."<br>";
+
+$units = 300;
+$bill = 0.0;
+
+function billCount($units){
+if($units != 0){
+    if($units < 51){
+        $bill = $units * 2.5;
+    }else{
+        $bill = 50 * 2.5;
+        if($units < 101 ){
+            $units -= 50;
+            $bill += $units * 5;
+        }else{
+            $bill += 50 * 5;
+            if($units < 251){
+                $units -= 100;
+                $bill += $units * 6.20;
+            }else{
+                $bill += 150 * 6.2;
+                if($units >= 251){
+                    $units -= 250;
+                    $bill += $units * 7.50;
+                }
+            }
+        }
+    }
+}
+return $bill;
+}
+echo billCount(300)."<br>";
+
+//********************************** */
+
+function vote($age){
+
+if($age < 18){
+    echo "is not eligible to vote <br>";
+}else{
+    echo "is eligible to vote <br>";
+}
+
+}
+
+vote(15);
+
+
+$firstInteger = 10;
+
+if($firstInteger == 0){
+    echo "zero <br>";
+}else if($firstInteger < 0){
+    echo "Negative <br>";
+}else {
+    echo "Postive <br>";
+}
+
+function calc($first,$second,$oper){
+switch ($oper) {
+    case '+':
+        $result = $first + $second;
+        break;
+    case '-':
+        $result = $first - $second;
+        break;
+    case '*':
+        $result = $first * $second;
+        break;
+    case '/':
+        $result = $first / $second;
+        break;
     
+    default:
+        # code...
+        break;
+}
+return $result;
+}
+
+echo calc(10,20,'*')."<br>";
+
+
+$nums = array(60,86,95,63,55,74,79,62,50);
+$sum = 0;
+for($i = 0; $i < count($numbers); $i++)
+    $sum += $numbers[$i];
+
+$avg = $sum / count($numbers);
+
+if($avg < 60){
+    echo "F";
+}else if($avg>60 && $avg <= 70){
+    echo "D";
+}else if($avg > 70 && $avg <= 80){
+    echo "C";
+}else if($avg > 80 && $avg <= 90){
+    echo "B";
+}else if($avg > 90 && $avg <= 100){
+    echo "A";
 }
